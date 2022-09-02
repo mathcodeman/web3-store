0fa3a1112c47f4afc76934b0ba233f06153f2858
Hins Guo <zongzhen@ualberta.ca>
2022-09-02 15:37:14 -0700

    authentication works without web3uikit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { ConnectButton } from 'web3uikit'
+import { useMoralis } from "react-moralis";
 
 export const Sidebar = () => {
+    const { authenticate, isAuthenticated, user } = useMoralis();
+
+    if (!isAuthenticated) {
+        return (
+            <div>
+                <button onClick={() => authenticate()}>Authenticate</button>
+            </div>
+        );
+    }
+
     return (
-        <div>Sidebar</div>
-    )
+        <div>
+            <h1>Welcome {user.get("username")}</h1>
+        </div>
+    );
 }
